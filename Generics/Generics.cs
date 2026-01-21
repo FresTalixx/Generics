0af223179200ca40de081daef2b550b2def16da3
frest <frest@DESKTOP-DFPD65U>
2026-01-21 10:33:59 +0200

    Add project files.

---
+++ Generics/Generics.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+class MathOperations<T> where T : struct, INumber<T>
+{
+    public static T MaxFromThree(T num1, T num2, T num3)
+    {
+        T max = num1;
+        if (max < num2) max = num2;
+        if (max < num3) max = num3;
+
+        return max;
+    }
+
+    public static T MinFromThree(T num1, T num2, T num3)
+    {
+        T min = num1;
+        if (min > num2) min = num2;
+        if (min > num3) min = num3;
+
+        return min;
+    }
+
+    public static T SumOfArray(T[] arr)
+    {
+        T sum = T.Zero;
+
+        foreach (T item in arr)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
+
+
+
+//Створіть generic-клас Стека. Реалізуйте стандартні методи та властивості для роботи стеку:
+//pop;
+//push;
+//peek;
+//count.
+
+
+
+class GenericStack<T> where T: struct, INumber<T>
+{
+    public T[] stack = Array.Empty<T>();
+
+    public void Push(T item)
+    {
+        stack = stack.Append(item).ToArray();
+    }
+
+    public T Pop()
+    {
+        T poppedElement = stack[stack.Length - 1];
+        stack = stack.Take(stack.Count() - 1).ToArray();
+        return poppedElement;
+    }
+
+    public T Peek()
+    {
+        return stack[stack.Length - 1];
+    }
+
+    public int Count()
+    {
+        return stack.Length;
+    }
+}
+
+
+public class Alphabet : IEnumerable<char>
+{
+    char[] alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    public IEnumerator<char> GetEnumerator()
+    {
+        foreach (char c in alphabet)
+        {
+            yield return c;
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+
+public static class ListSort<T> where T: struct
+{
+    public static List<T> FilterByTwoCriteria(List<T> list, Predicate<T> predicate1,
+        Predicate<T> predicate2)
+    {
+        List<T> result = new List<T>();
+
+        foreach (T item in list)
+        {
+            if (predicate1(item) && predicate2(item))
+            {
+                result.Add(item);
+            }
+        }
+        return result;
+    }
+}
+
+public class Resident
+{
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+    public required string Job { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}, Job: {Job}";
+    }
+}
+
+
+public class Apartment : IEnumerable<string>
+{
+    public required Resident[] Residents { get; set; }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        foreach (var resident in Residents)
+        {
+            yield return resident.ToString();
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override string ToString()
+    {
+        string finalString = "";
+        foreach (var resident in Residents)
+        {
+            finalString += resident.ToString() + " ";
+        }
+        return finalString;
+    }
+}
+
+
+
+public class Building : IEnumerable<string>
+{
+
+    public required Apartment[] Apartments { get; set; }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        for (int i = 0; i < Apartments.Length; ++i)
+        {
+            yield return  $"Apartment {i + 1}: {Apartments[i].ToString()}";
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+
+public interface IBook
+{
+    public string ReleaseDate { get; set; }
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+}
+
+
+public class EBook : IBook
+{
+    public string ReleaseDate { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+
+    public int FontSize { get; set; }
+
+    public override string ToString()
+    {
+        return $"Author: {Author}, Release Date: {ReleaseDate}, Title: {Title}, Genre: {Genre}, Font Size: {FontSize}";
+    }
+}
+
+public class PrintedBook : IBook
+{
+    public string ReleaseDate { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public bool isNew { get; set; }
+
+    public override string ToString()
+    {
+        return $"Author: {Author}, Release Date: {ReleaseDate}, Title: {Title}, Genre: {Genre}, Is New: {isNew}";
+    }
+}
+
+public class Library<T> : IEnumerable<T> where T: class, IBook
+{
+    public List<T> bookList = new List<T>();
+
+    public void AddBook(T book)
+    {
+        bookList.Add(book);
+    }
+
+    public void RemoveBook(T book)
+    {
+        bookList.Remove(book);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var book in bookList)
+        {
+            yield return book;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public List<T> SortBooksByCriteria(Predicate<T> criteria)
+    {
+        List<T> newBookList = new List<T>();
+
+        foreach (var book in bookList)
+        {
+            if (criteria(book)) newBookList.Add(book);
+        }
+
+        return newBookList;
+    }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,174 @@
+﻿
+double num1 = 10.1;
+double num2 = 20.34;
+double num3 = 15.2;
+
+Console.WriteLine("Max of three integers: " + MathOperations<double>.MaxFromThree(num1, num2, num3));
+
+Console.WriteLine("Min of three integers: " + MathOperations<double>.MinFromThree(num1, num2, num3));
+
+
+double[] arr = { 10.5, 20.3, 30.2, 40.4, 50.1 };
+
+Console.WriteLine("Sum of array elements: " + MathOperations<double>.SumOfArray(arr));
+
+var stack = new GenericStack<double>();
+
+stack.Push(num1);
+stack.Push(num2);
+stack.Push(num3);
+
+Console.WriteLine($"Stack Length: {stack.Count()}");
+
+Console.WriteLine($"Stack popped: {stack.Pop()}");
+Console.WriteLine($"Stack Length: {stack.Count()}");
+
+Console.WriteLine($"Stack peeked: {stack.Peek()}");
+Console.WriteLine($"Stack Length: {stack.Count()}");
+
+
+
+var alphabet = new Alphabet();
+
+foreach ( char c in alphabet )
+{
+    Console.Write($"{c} ");
+}
+
+
+
+Console.WriteLine("Task6");
+
+List<int> list1 = new List<int>() { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+
+Predicate<int> isEvenPredicate = (int num1) => num1 % 2 == 0;
+Predicate<int> isNegativePredicate = (int num2) => num2 >= 0;
+
+List<int> newList = ListSort<int>.FilterByTwoCriteria(list1, isEvenPredicate, isNegativePredicate);
+
+foreach ( int i in newList )
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+
+var resident1 = new Resident()
+{
+    Name = "John",
+    Age = 26,
+    Job = "Chief of police"
+};
+
+
+var resident2 = new Resident()
+{
+    Name = "Alice",
+    Age = 22,
+    Job = "Builder"
+};
+
+
+var resident3 = new Resident()
+{
+    Name = "Olivia",
+    Age = 12,
+    Job = "Student"
+};
+
+var resident4 = new Resident()
+{
+    Name = "William",
+    Age = 44,
+    Job = "Developer"
+};
+
+var resident5 = new Resident()
+{
+    Name = "Mark",
+    Age = 34,
+    Job = "Supervisor"
+};
+
+Resident[] apartment1Residents = [resident1, resident2];
+Resident[] apartment2Residents = [resident3, resident4];
+Resident[] apartment3Residents = [resident5];
+
+var apartment1 = new Apartment()
+{
+    Residents = apartment1Residents
+};
+
+var apartment2 = new Apartment()
+{
+    Residents = apartment2Residents
+};
+
+var apartment3 = new Apartment()
+{
+    Residents = apartment3Residents
+};
+
+Apartment[] buildingApartments = [apartment1, apartment2, apartment3];
+
+var building = new Building()
+{
+    Apartments = buildingApartments
+};
+
+foreach (var apart in building)
+{
+    Console.WriteLine(apart);
+}
+
+
+var book1 = new EBook()
+{
+    Title = "The Great Gatsby",
+    Author = "F. Scott Fitzgerald",
+    ReleaseDate = "1925",
+    Genre = "Novel",
+    FontSize = 12
+};
+
+var book2 = new EBook()
+{
+    Title = "1984",
+    Author = "George Orwell",
+    ReleaseDate = "1949",
+    Genre = "Dystopian",
+    FontSize = 14
+};
+
+var book3 = new EBook()
+{
+    Title = "To Kill a Mockingbird",
+    Author = "Harper Lee",
+    ReleaseDate = "1960",
+    Genre = "Southern Gothic",
+    FontSize = 14
+};
+
+
+var ELirary = new Library<EBook>();
+
+ELirary.AddBook(book1);
+ELirary.AddBook(book2);
+ELirary.AddBook(book3);
+
+foreach (var book in ELirary)
+{
+    Console.WriteLine(book);
+}
+
+Predicate<EBook> sortByFont14 = (EBook book) => book.FontSize == 14;
+
+List<EBook> books = ELirary.SortBooksByCriteria(sortByFont14);
+
+Console.WriteLine();
+
+foreach (EBook book in books)
+{
+    Console.WriteLine(book);
+}
